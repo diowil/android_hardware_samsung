8b2ea552b07c50a282932061f25854d2a602b0e5
Christopher N. Hesse <raymanfx@gmail.com>
2016-02-22 21:57:39 +0100

    libril: Make ST-E Thor M7450 and Intel XMM7260 next gen modems as well
    Change-Id: I7cacda8cdaf9e285688c06c42ccf6629efc2af9d

--- ril/libril/ril.cpp
+++ ril/libril/ril.cpp
@@ -792,8 +792,7 @@ dispatchDial (Parcel &p, RequestInfo *pRI) {
     int32_t sizeOfDial;
     int32_t t;
     int32_t uusPresent;
-#if defined(MODEM_TYPE_XMM7260) || defined(MODEM_TYPE_M7450) \
-  || defined(SAMSUNG_NEXT_GEN_MODEM)
+#ifdef SAMSUNG_NEXT_GEN_MODEM
     char *csv;
 #endif
     status_t status;
@@ -809,8 +808,7 @@ dispatchDial (Parcel &p, RequestInfo *pRI) {
         goto invalid;
     }
 
-#if defined(MODEM_TYPE_XMM7260) || defined(MODEM_TYPE_M7450) \
-  || defined(SAMSUNG_NEXT_GEN_MODEM)
+#ifdef SAMSUNG_NEXT_GEN_MODEM
     /* CallDetails.call_type */
     status = p.readInt32(&t);
     if (status != NO_ERROR) {
@@ -840,8 +838,7 @@ dispatchDial (Parcel &p, RequestInfo *pRI) {
         }
 
         if (uusPresent == 0) {
-#if defined(MODEM_TYPE_XMM6262) || defined(MODEM_TYPE_XMM7260) \
- || defined(MODEM_TYPE_M7450) || defined(SAMSUNG_NEXT_GEN_MODEM)
+#if defined(MODEM_TYPE_XMM6262) || defined(SAMSUNG_NEXT_GEN_MODEM)
             dial.uusInfo = NULL;
 #elif defined(MODEM_TYPE_XMM6260)
             /* Samsung hack */
@@ -2379,11 +2376,11 @@ static int responseCallList(Parcel &p, void *response, size_t responselen) {
         p.writeInt32(p_cur->als);
         p.writeInt32(p_cur->isVoice);
 
-#if defined(MODEM_TYPE_XMM7260) || defined(MODEM_TYPE_M7450) || defined(SAMSUNG_NEXT_GEN_MODEM)
-#ifndef SAMSUNG_NEXT_GEN_MODEM
+#ifdef NEEDS_VIDEO_CALL_FIELD
         p.writeInt32(p_cur->isVideo);
 #endif
 
+#ifdef SAMSUNG_NEXT_GEN_MODEM
         /* Pass CallDetails */
         p.writeInt32(0);
         p.writeInt32(0);
@@ -2418,7 +2415,7 @@ static int responseCallList(Parcel &p, void *response, size_t responselen) {
             p_cur->als,
             (p_cur->isVoice)?"voc":"nonvoc",
             (p_cur->isVoicePrivacy)?"evp":"noevp");
-#if defined(MODEM_TYPE_XMM7260) || defined(MODEM_TYPE_M7450) || defined(SAMSUNG_NEXT_GEN_MODEM)
+#ifdef SAMSUNG_NEXT_GEN_MODEM
         appendPrintBuf("%s,%s,",
             printBuf,
             (p_cur->isVideo) ? "vid" : "novid");
@@ -3021,8 +3018,7 @@ static int responseRilSignalStrength(Parcel &p,
 
         p.writeInt32(p_cur->GW_SignalStrength.bitErrorRate);
 
-#if defined(MODEM_TYPE_XMM6262) || defined(MODEM_TYPE_XMM7260) \
- || defined(MODEM_TYPE_M7450) || defined(SAMSUNG_NEXT_GEN_MODEM)
+#if defined(MODEM_TYPE_XMM6262) || defined(SAMSUNG_NEXT_GEN_MODEM)
         cdmaDbm = p_cur->CDMA_SignalStrength.dbm & 0xFF;
         if (cdmaDbm < 0) {
             cdmaDbm = 99;
@@ -3035,8 +3031,7 @@ static int responseRilSignalStrength(Parcel &p,
         p.writeInt32(cdmaDbm);
         p.writeInt32(p_cur->CDMA_SignalStrength.ecio);
 
-#if defined(MODEM_TYPE_XMM6262) || defined(MODEM_TYPE_XMM7260) \
- || defined(MODEM_TYPE_M7450) || defined(SAMSUNG_NEXT_GEN_MODEM)
+#if defined(MODEM_TYPE_XMM6262) || defined(SAMSUNG_NEXT_GEN_MODEM)
         evdoDbm = p_cur->EVDO_SignalStrength.dbm & 0xFF;
         if (evdoDbm < 0) {
             evdoDbm = 99;
